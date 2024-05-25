ac71e01add0bc81a8842df2cec4bef6b458bbe0d
Андрій Іл'ющенко <137763905+asiliushchenko@users.noreply.github.com>
2024-05-25 14:02:56 +0300

    Add files via upload

---
+++ WindowsFormsApp1.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WindowsFormsApp1.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WindowsFormsApp1.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ca03c7a9-51e3-4bf5-977d-47dbac889c1b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WindowsFormsApp1.Tests/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Windows.Forms;
+using WindowsFormsApp1;
+
+namespace WindowsFormsApp1.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        // Тест додавання запису
+        [TestMethod]
+        public void TestAddDocument()
+        {
+            Form1 form = new Form1();
+            int initialRowCount = form.dataGridView1.RowCount;
+            form.Add_document_DataEntered(this, new DataEnteredEventArgs("1", "0050359753", "10/6/2021", "ДТЕК", "9/14/2021", "12/31/2021", "120,108.56", "Приєднання до електромережі"));
+            Assert.AreEqual(initialRowCount + 1, form.dataGridView1.RowCount);
+        }
+
+        // Тест видалення запису
+        [TestMethod]
+        public void TestDeleteDocument()
+        {
+            Form1 form = new Form1();
+            int initialRowCount = form.dataGridView1.RowCount;
+            form.dataGridView1.Rows.Add("9", "70/21", "11/10/2021", "ФОП Ківаркова С.О.", "11/10/2021", "12/31/2021", "4,000.00", "Ролеки");
+            form.dataGridView1.Rows[0].Selected = true;
+            form.DeleteDocument_Click(this, EventArgs.Empty);
+            Assert.AreEqual(initialRowCount, form.dataGridView1.RowCount);
+        }
+    }
+}
